27bc09766c33811fce281b3aab7e6319f8518800
markimoo09 <99109211+markimoo09@users.noreply.github.com>
2022-09-29 15:31:06 +0800

    Publishing Trivia App

---
+++ TrivApp/app/src/main/java/com/example/android/trivapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.android.trivapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.trivapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFrag)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFrag)
+        return navController.navigateUp()
+    }
+}
